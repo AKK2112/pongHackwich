bcb20e68afb920c5ad73ca3a29d1d342f559fb41
Alec Kinzie <aleckk18@gmail.com>
2022-04-20 13:32:19 -0500

    final product

--- pongHackwich/GameScene.swift
+++ pongHackwich/GameScene.swift
@@ -12,12 +12,13 @@ var playerOnePaddle = SKSpriteNode()
 var ball = SKSpriteNode()
 
 var scoreLabel = SKLabelNode()
+var deathsLabel = SKLabelNode()
+var infoLabel = SKLabelNode()
 
-var score = 0 {
-    didSet {
-        scoreLabel.text = "Score: \(score)"
-    }
-}
+
+var score = 0
+var deaths = 0
+
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
@@ -36,11 +37,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bottom.physicsBody?.contactTestBitMask = 1
         addChild(bottom)
         
-        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel = self.childNode(withName: "scorer") as! SKLabelNode
         scoreLabel.text = "Score: 0"
-        scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.position = CGPoint(x: 500, y: 500)
-        addChild(scoreLabel)
+        
+        deathsLabel = self.childNode(withName: "deaths") as! SKLabelNode
+        deathsLabel.text = "Deaths: 0"
+        
+        infoLabel = self.childNode(withName: "info") as! SKLabelNode
+        infoLabel.text = "Don't hit the bottom more than 3 times!"
+        //        scoreLabel.horizontalAlignmentMode = .right
+        //        scoreLabel.position = CGPoint(x: 500, y: 500)
+        //        addChild(scoreLabel)
+    }
+    
+    struct game {
+        static var IsOver : Bool = false
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,21 +60,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         playerOnePaddle.run(SKAction.moveTo(x: point.x, duration: 0.2))
         
     }
-   
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {return}
         let point = touch.location(in: self)
         playerOnePaddle.run(SKAction.moveTo(x: point.x, duration: 0.2))
     }
     
+    
+    
     func didBegin(_ contact: SKPhysicsContact) {
         print(contact.bodyA)
         print(contact.bodyB)
+        if contact.bodyA.node?.name == "playerOnePaddle" && contact.bodyB.node?.name == "Ball" {
+            score += 1
+            scoreLabel.text = "Score: \(score)"
+        }
+        
         if contact.bodyA.node?.name == "bottom" && contact.bodyB.node?.name == "Ball" {
+            deaths += 1
+            deathsLabel.text = "Deaths: \(deaths)"
+        }
+        
+        if deaths == 3 {
             ball.removeFromParent()
             playerOnePaddle.removeFromParent()
-            score += 1
+            infoLabel.text = "You hit the bottom more than 3 times! oops!"
+            scoreLabel.text = "Game over"
+            deathsLabel.text = "You Loose"
         }
     }
-
+    
+    
+    
 }
